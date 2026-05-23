becbf922ee38b6df62cee3a1f3d2bbbf47b36cf3
Sai Teja <as24csb0b07@student.nitw.ac.in>
2026-05-23 17:40:48 +0530

    Update default page

--- app.js
+++ app.js
@@ -97,9 +97,9 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root.");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
